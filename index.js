7adb944abaed83044dec4bed17b11d3da9ee9290
deendi1 <deendi73@gmail.com>
2024-07-17 09:14:53 +0700

    Perubahan 2

--- index.js
+++ index.js
@@ -10,5 +10,40 @@ function init() {
     );
     }, 1000)
 }
-
 window.addEventListener('load', init, false);
+
+src="https://code.jquery.com/jquery-3.1.1.min.js"
+crossorigin="anonymous"
+
+function getStoredValue() {
+    conts StoredValue =
+    localStorage.getItem('counterValue')
+    return StoredValue === null
+    ? 0
+    : Number(StoredValue);
+}
+
+function handleClickCounter() {
+    const StoredValue =  getStoredValue();
+    const newValue = StoredValue + 1;
+    // update button text
+    const elButton =
+    document.getElementById('btnCounter')
+    elButton.innerHTML = newValue;
+    // Store new value
+    localStorage.setItem('counterValue', newValue);
+}
+
+$(document).ready(() => {
+    const elButton =
+    document.getElementById('btnCounter');
+    elButton.innerHTML = getStoredValue();
+});
+
+//const elBtnHello = document.getElementById('btnHello');
+//const sayHello = () => alert('Hello');
+
+// Menambahkan event handler sendiri
+//elBtnHello.addEventListener('click', sayHello)
+
+
